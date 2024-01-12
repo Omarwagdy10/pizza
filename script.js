6163b1893b04f1ffea1e1e2ef09ee91898787b1d
omar wagdy <omarwagdy122@gmail.com>
2024-01-12 16:55:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function hide (){
+    let headerRs1 = document.querySelector(".header-rs1")
+    headerRs1.classList.toggle("hide")
+}
+
+
+function showMin (){
+    let SmallMen = document.querySelector(".small-men")
+    SmallMen.classList.toggle("hideMen")
+}
+
+function showmin2(){
+    let list2 = document.querySelector(".list2")
+    list2.classList.toggle("hide-list2")
+}
